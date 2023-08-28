9abc4547e9b3306ac18ad39e5d4e1f8961d8e7f0
Michel Vidailhet <michel.vidailhet@gmail.com>
2023-08-28 13:51:32 +0200

    Affichage des noms des 10 premiers pokémons affichager des détails du pokémon Pikachu

--- script.js
+++ script.js
@@ -1,1 +1,21 @@
-console.log('hello');
+
+const listUrl = 'https://pokeapi.co/api/v2/pokemon?limit=10';
+
+
+// Affichage des noms des 10 premiers pokémons
+
+fetch(listUrl)
+.then((response) => response.json())
+.then((listData) => {
+  console.log(listData);
+  const list = listData.results;
+  list.forEach(pokemon => {
+    console.log(pokemon.name);
+  });
+})
+
+// affichager des détails du pokémon Pikachu
+const url = 'https://pokeapi.co/api/v2/pokemon/pikachu';
+fetch(url)
+.then((response) => response.json())
+.then(data => console.log(data));
